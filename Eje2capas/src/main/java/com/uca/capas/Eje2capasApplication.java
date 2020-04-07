c09bef15e6914c1250ede34787b51fa86620193f
David <00184217@uca.edu.sv>
2020-04-06 18:19:55 -0600

    listo

---
+++ Eje2capas/src/main/java/com/uca/capas/Eje2capasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eje2capasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eje2capasApplication.class, args);
+	}
+
+}
